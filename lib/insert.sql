7b31439bc412429127bc72c8f568b5d9c7b12fb7
Gi-thinji <githinji.ngare@gmail.com>
2023-08-30 19:13:28 +0300

    passed tests

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (
+name,
+age,
+sex,
+color,
+temperament,
+alive)
+VALUES ('Mr. Chocolate', 12, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 5, 'M', 'Black', 'Aggressive', 0),
+('Tabitha', 9, 'F', 'White', 'Playful', 1),
+('Sergeant Brown', 7, 'M', 'Brown', 'Grumpy', 1),
+('Melissa', 18, 'F', 'Black', 'Calm', 0),
+('Grinch', 3, 'M', 'White', 'Playful', 1),
+('Wendy', 14, 'F', 'Brown', 'Grumpy', 1),
+(NULL, 21, 'F', 'Other', 'Aggressive', 0);
